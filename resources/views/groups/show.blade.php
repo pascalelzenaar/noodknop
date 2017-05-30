fec2a07cef7b1109dcc73f6f6b7cebda79d2f9d0
pascalelzenaar <pascalelzenaar@gmail.com>
2017-05-30 14:00:09 +0200

    Laravel geupload

---
+++ resources/views/groups/show.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10">
+
+                <h1>{{ $group->name }}</h1>
+
+            </div>
+            <div class="col-md-2">
+                
+            </div>
+        </div>
+    </div>
+@endsection
